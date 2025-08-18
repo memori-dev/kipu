c11d25a65e8475e6db122e8189ef6b3a7ed0196b
memori <32135165+memori-dev@users.noreply.github.com>
2025-08-18 10:21:36 -0400

    createModule -> addModule to make it public, removed addLibrary

--- build.zig
+++ build.zig
@@ -4,26 +4,15 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const root_source_file = b.path("src/root.zig");
-    const install_step = b.getInstallStep();
-
-    const lib_mod = b.createModule(.{
-        .root_source_file = root_source_file,
+    const lib_mod = b.addModule("kipu", .{
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    const lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "kipu",
-        .root_module = lib_mod,
-    });
-
-    b.installArtifact(lib);
 
     const tests = b.addTest(.{.root_module = lib_mod});
     const tests_run = b.addRunArtifact(tests);
     const tests_step = b.step("test", "Run tests");
     tests_step.dependOn(&tests_run.step);
-    install_step.dependOn(tests_step);
+    b.getInstallStep().dependOn(tests_step);
 }
